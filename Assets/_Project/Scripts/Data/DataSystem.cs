81502721584b615f690d3f5903f2ea9a00f7d118
ayuki <3271978170@qq.com>
2026-03-19 01:48:18 +0800

    feat: 添加底层的DataSystem
    还把Scenes的文件夹移动到_Project内
    
    游戏存档和设置保存还没有写

---
+++ Assets/_Project/Scripts/Data/DataSystem.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public static class DataPath
+{
+    private const string FolderName = ".userdata";
+
+    public static string GetRoot()
+    {
+        string baseDir = Directory.GetParent(Application.dataPath).FullName;
+        string subPath = Application.isEditor ? "Temp" : "";
+        string finalPath = Path.Combine(baseDir, subPath, FolderName);
+
+        // 动态创建
+        if (!Directory.Exists(finalPath))
+        {
+            DirectoryInfo di = Directory.CreateDirectory(finalPath);  // 创建文件夹
+            // 打包后的版本执行系统隐藏
+            /*
+            if (!Application.isEditor && Application.platform == RuntimePlatform.WindowsPlayer)
+             {
+                 di.Attributes = FileAttributes.Directory | FileAttributes.Hidden | FileAttributes.System;
+            }
+            */
+        }
+
+        return finalPath;
+    }
+}
+
+public static class DataSystem
+{
+    public static void SaveData<T>(string saveFileName, T data)
+    {
+        string j = JsonUtility.ToJson(data, true);
+        string p = Path.Combine(DataPath.GetRoot(), saveFileName);
+
+        File.WriteAllText(p, j);
+    }
+
+    public static T LoadData<T>(string saveFileName) where T : new()
+    {
+        string p = Path.Combine(DataPath.GetRoot(), saveFileName);
+
+        if (!File.Exists(p))
+        {
+            Debug.LogWarning($"[DataSystem] 未找到数据: {saveFileName}");
+            return new T();
+        }
+
+        string j = File.ReadAllText(p);
+        T data = JsonUtility.FromJson<T>(j);
+        return data;
+    }
+}
+
