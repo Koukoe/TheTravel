2145c0c0370cd095d66c0dea65123f3c1e343719
ayuki <3271978170@qq.com>
2026-03-30 23:20:58 +0800

    feat: 添加按钮状态更新

--- Assets/_Project/Scripts/UI/UIFrame/UIListener.cs
+++ Assets/_Project/Scripts/UI/UIFrame/UIListener.cs
@@ -1,21 +1,11 @@
 using System;
 using UnityEngine;
 
-public abstract class UIListener : MonoBehaviour, IUIListener
+public abstract class UIListener : MonoBehaviour
 {
     public abstract void Open();
     public abstract void Resume();
     public abstract void Close(Action onFinished);
     public abstract void Suspend(Action onFinished);
     public abstract void Abort();
-}
-
-public interface IUIListener
-{
-    void Open();
-    void Resume();
-    void Close(Action onFinished);
-    void Suspend(Action onFinished);
-
-    void Abort();
 }
