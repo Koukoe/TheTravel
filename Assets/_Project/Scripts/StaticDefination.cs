b8d1240b77c842326bf7d72addcc09de3fdd3e5a
rechenz <13244571368@qq.com>
2026-04-03 15:07:59 +0800

    fix:调整shipInside场景的灯光颜色，为StaticDefination脚本添加注释

--- Assets/_Project/Scripts/StaticDefination.cs
+++ Assets/_Project/Scripts/StaticDefination.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class StaticDefination
 {
     [SerializeField]
-    public static Vector3 CameraPos = new Vector3(0, 10, 0);
-    public static Vector3 CameraRot = new Vector3(45, -45, 0);
-    public static float CameraY = 10;
+    public static Vector3 CameraPos = new Vector3(0, 10, 0);//定义相机位置
+    public static Vector3 CameraRot = new Vector3(45, -45, 0);//定义相机旋转
+    public static float CameraY = 10;//定义相机Y轴高度差
 }
