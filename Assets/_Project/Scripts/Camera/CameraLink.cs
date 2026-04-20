b5694c694628acd9ea36cf992f923c1f2db7ae14
ayuki <3271978170@qq.com>
2026-04-20 20:21:16 +0800

    feat: 编写 CameraLink.cs 以动态链接 MainCamera

---
+++ Assets/_Project/Scripts/Camera/CameraLink.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CameraLink : MonoBehaviour
+{
+    public Camera gameUICamera;
+    public Camera sysUICamera;
+    public Canvas gameWorldCanvas;
+
+    public static CameraLink Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void LinkToMainCamera()
+    {
+        Camera mainCam = Camera.main;
+        if (mainCam != null)
+        {
+            var cameraData = mainCam.GetUniversalAdditionalCameraData();
+            if (cameraData == null) return;
+            if (gameUICamera != null && !cameraData.cameraStack.Contains(gameUICamera))
+            {
+                cameraData.cameraStack.Add(gameUICamera);
+            }
+            if (sysUICamera != null && !cameraData.cameraStack.Contains(sysUICamera))
+            {
+                cameraData.cameraStack.Add(sysUICamera);
+            }
+            if (gameWorldCanvas != null)
+                gameWorldCanvas.worldCamera = mainCam;
+        }
+    }
+}
