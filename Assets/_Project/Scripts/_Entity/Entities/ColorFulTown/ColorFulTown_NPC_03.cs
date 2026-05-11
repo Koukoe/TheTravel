d09b05d156ea24f14be8dc13b04b408e771152be
rechenz <13244571368@qq.com>
2026-05-11 16:22:17 +0800

    add:制作三个colorfultown的npc

---
+++ Assets/_Project/Scripts/_Entity/Entities/ColorFulTown/ColorFulTown_NPC_03.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFulTown_NPC_03 : StaticStateEntity<ActorState>
+{
+    protected override void OnStateBound()
+    {
+        if (_state.position != null) gameObject.transform.position = _state.position.Value;
+        if (_state.rotation != null) gameObject.transform.rotation = Quaternion.Euler(_state.rotation.Value);
+        if (_state.isVisible)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
