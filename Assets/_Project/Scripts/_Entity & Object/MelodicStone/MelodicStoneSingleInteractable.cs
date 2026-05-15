660fa161ad6ef375e95baa6dfab3f4a34abb2852
ayuki <3271978170@qq.com>
2026-05-15 23:49:34 +0800

    摆了（摆了物体嗯

--- Assets/_Project/Scripts/_Entity & Object/MelodicStone/MelodicStoneSingleInteractable.cs
+++ Assets/_Project/Scripts/_Entity & Object/MelodicStone/MelodicStoneSingleInteractable.cs
@@ -4,7 +4,7 @@ using Unity.IO.LowLevel.Unsafe;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class radioKnobFreq : MonoBehaviour, IInteractable
+public class MelodicStoneSingleInteractable : MonoBehaviour, IInteractable
 {
 
     [SerializeField] private int _priority;
